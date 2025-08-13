8f0002c1a2b2065b08c796c0aa17830f911d4053
Soham Bodhani <sohambodhani994@gmail.com>
2025-08-13 08:19:17 +0530

    updated screeens

---
+++ birthday_card/lib/screens/second_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key, required this.name});
+
+  final String name;
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Second Screen')),
+      body: Center(
+        child: Text(
+          'Welcome ${widget.name} to Second Screen',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
